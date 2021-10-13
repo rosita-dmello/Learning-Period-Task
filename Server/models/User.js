9eaede42a851eb18669a707e6e2122ffb92b2475
Rosita <esorthe7@gmail.com>
2021-10-13 16:35:55 +0530

    Add role to user

--- Server/models/User.js
+++ Server/models/User.js
@@ -17,14 +17,16 @@ const userSchema = new mongoose.Schema({
     // required: [true, 'Please enter an email'],
     // unique: true,
     // lowercase: true,
-    // validate: [isEmail, 'Please enter a valid email']
+    validate: [isEmail, 'Please enter a valid email']
   },
   password: {
     type: String,
     // required: [true, 'Please enter a password'],
     // minlength: [6, 'Minimum password length is 6 characters'],
+    validate: [isStrongPassword, ]
   },
-  googleId: String
+  googleId: String,
+  role: String
 });
 
 userSchema.plugin(findOrCreate);
